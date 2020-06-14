ebf83e4e28cb429df6ae7b871cc39afc600a85e5
Davies Esiro <ojurereoluwad@gmail.com>
2020-06-14 16:59:25 +0100

    Added cloud functions for making a user admin

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,55 +1,102 @@
-//listen for auth status changes
+// add admin cloud function
+const adminForm = document.querySelector('.admin-actions');
+adminForm.addEventListener('submit', e => {
+    e.preventDefault();
+    adminEmail = document.querySelector('#admin-email').value;
+    console.log(adminEmail);
+    const addAdminRole = functions.httpsCallable('addAdminRole');
+    addAdminRole({ email: adminEmail }).then(result =>{
+        console.log(result);
+    });
+})
+
+// listen for auth status changes
 auth.onAuthStateChanged(user => {
-    setUpUI(user);
-    if(user){
-        //get data
-        db.collection('guides').get().then(snapshot => {
-            setupGuides(snapshot.docs); // from the index.js
-        });
-        
+    if (user) {
+        user.getIdTokenResult().then(idTokenResult => {
+            console.log(idTokenResult.claims);
+        })
+        db.collection('guides').onSnapshot(snapshot => {
+            setupGuides(snapshot.docs);
+            setupUI(user);
+        }, err => console.log(err.message));
     } else {
+        setupUI();
         setupGuides([]);
-        
     }
 });
 
-//signup
-const signupForm = document.querySelector('#signup-form');
-signupForm.addEventListener('submit', e => {
+// create new guide
+const createForm = document.querySelector('#create-form');
+createForm.addEventListener('submit', (e) => {
     e.preventDefault();
-
-    //get user info
-    const email = signupForm['signup-email'].value;
-    const password = signupForm['signup-password'].value;
-
-    //sign up the user
-    auth.createUserWithEmailAndPassword(email, password).then(cred => {
-        const modal = document.querySelector('#modal-signup');
-        M.Modal.getInstance(modal).close();
-        signupForm.reset();
+    db.collection('guides').add({
+    title: createForm.title.value,
+    content: createForm.content.value
+    }).then(() => {
+    // close the create modal & reset form
+    const modal = document.querySelector('#modal-create');
+    M.Modal.getInstance(modal).close();
+    createForm.reset();
+    }).catch(err => {
+    console.log(err.message);
     });
 });
 
-//logout
+// signup
+const signupForm = document.querySelector('#signup-form');
+signupForm.addEventListener('submit', async(e) => {
+    e.preventDefault();
+
+  // get user info
+    const email = signupForm['signup-email'].value;
+    const password = signupForm['signup-password'].value;
+
+    await createUserDocument(email, password);
+
+    // closing modal
+    const modal = document.querySelector('#modal-signup');
+    M.Modal.getInstance(modal).close();
+    signupForm.reset();
+});
+
+//create user function
+const createUserDocument = async(email, password) => {
+    try {
+        //getting user from the userAuth
+        const { user } = await auth.createUserWithEmailAndPassword(email, password);
+        const userRef = db.collection('users').doc(user.uid);
+        userRef.set({
+            bio: signupForm['signup-bio'].value
+        });
+
+    } catch (error) {
+        console.log(error.message)
+    }
+}
+
+// logout
 const logout = document.querySelector('#logout');
 logout.addEventListener('click', (e) => {
     e.preventDefault();
     auth.signOut();
 });
 
-//login
+// login
 const loginForm = document.querySelector('#login-form');
-loginForm.addEventListener('submit', e => {
-    e.preventDefault(); 
+loginForm.addEventListener('submit', (e) => {
+    e.preventDefault();
 
-    //get user info
+  // get user info
     const email = loginForm['login-email'].value;
     const password = loginForm['login-password'].value;
 
-    auth.signInWithEmailAndPassword(email, password).then(cred => {
-        //close the login modal and reset the form
-        const modal = document.querySelector('#modal-login');
-        M.Modal.getInstance(modal).close();
-        loginForm.reset();
-    })
+  // log the user in
+    auth.signInWithEmailAndPassword(email, password).then((cred) => {
+    // close the signup modal & reset form
+    const modal = document.querySelector('#modal-login');
+    M.Modal.getInstance(modal).close();
+    loginForm.reset();
+});
+
 });
